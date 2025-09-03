ee723508b0f82f9fe00474df5ed4de97be4ca5da
KoruptTinker <pisces1237@gmail.com>
2025-09-03 16:12:25 -0400

    Update: Physics system to be more generic

--- src/Physics.h
+++ src/Physics.h
@@ -2,15 +2,8 @@
 #include "Entity.h"
 
 class PhysicsSystem {
-private:
-  float gravity;
-
 public:
-  PhysicsSystem() : gravity(980.0f) {} // Default gravity (pixels/second^2)
-
-  void SetGravity(float value) { gravity = value; }
-  float GetGravity() const { return gravity; }
+  PhysicsSystem() = default;
 
   void ApplyPhysics(Entity *entity, float deltaTime);
-  void ApplyGravity(Entity *entity, float deltaTime);
 };
