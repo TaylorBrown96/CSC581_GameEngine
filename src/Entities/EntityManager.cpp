f518ecfba6860e5a9702d307c02b3d6924b1c3e6
KoruptTinker <pisces1237@gmail.com>
2025-11-06 21:14:14 -0500

    Fix: Segfault issue

--- src/Entities/EntityManager.cpp
+++ src/Entities/EntityManager.cpp
@@ -5,15 +5,13 @@
 void EntityManager::AddEntity(Entity *entity) { entities.push_back(entity); }
 
 void EntityManager::RemoveEntity(Entity *entity) {
-  delete entity;
+
   entities.erase(std::remove(entities.begin(), entities.end(), entity),
                  entities.end());
+  delete entity;
 }
 
 void EntityManager::ClearAllEntities() {
-  for (int i = 0; i < entities.size(); i++) {
-    delete entities[i];
-  }
   entities.clear(); 
 }
 
