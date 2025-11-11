ab7685f9b3ea1e087364798daa6bf20a83fc5c7a
TaylorBrown96 <Taylor.j.brown1996@Gmail.com>
2025-11-10 19:16:33 -0500

    Fixed windows build version & added continous recording

--- src/Events/EventTypes.h
+++ src/Events/EventTypes.h
@@ -1,10 +1,10 @@
 #pragma once
+#include <climits>
 
 enum EventType {
-    EVENT_TYPE_INPUT = __INT_MAX__ - 1,
-    EVENT_TYPE_COLLISION = __INT_MAX__ - 2,
-    EVENT_TYPE_SPAWN = __INT_MAX__ - 3,
-    EVENT_TYPE_REPLAY_START = __INT_MAX__ - 4,
-    EVENT_TYPE_REPLAY_STOP = __INT_MAX__ - 5
-
+    EVENT_TYPE_INPUT = INT_MAX - 1,
+    EVENT_TYPE_COLLISION = INT_MAX - 2,
+    EVENT_TYPE_SPAWN = INT_MAX - 3,
+    EVENT_TYPE_REPLAY_START = INT_MAX - 4,
+    EVENT_TYPE_REPLAY_STOP = INT_MAX - 5
 };
